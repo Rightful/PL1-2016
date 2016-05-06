1ba4e7844c48a4639e93e74984735923099149bc
tadzjibov <k.z.tadzjibov@student.tudelft.nl>
2016-05-06 19:11:03 +0200

    New positioning method

--- src/main/java/io/github/programminglife2016/pl1_2016/parser/PositionHandler.java
+++ src/main/java/io/github/programminglife2016/pl1_2016/parser/PositionHandler.java
@@ -1,10 +1,5 @@
 package io.github.programminglife2016.pl1_2016.parser;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Random;
-
 /**
  * Handler for nodes to calculate positions on the graph.
  */
@@ -13,17 +8,20 @@ public class PositionHandler implements PositionManager {
      * Spacing between segments in the graph.
      */
     private static final int SPACING = 100;
+    private static final String MAIN_GENOME = "MT_H37RV_BRD_V5.ref.fasta";
 
     /**
      * Map containing the DNA seqments.
      */
     private NodeCollection nodeCollection;
+    private IGenome[] genomes;
 
     /**
      * Create a PositionHandler.
      * @param nodeCollection Map containing all the segments which positions need to be calculated.
      */
-    public PositionHandler(NodeCollection nodeCollection) {
+    public PositionHandler(NodeCollection nodeCollection, IGenome[] genomes) {
+        this.genomes = genomes;
         this.nodeCollection = new NodeList(nodeCollection.getNodes().size());
         for (Node node : nodeCollection) {
             this.nodeCollection.put(node.getId(), node);
@@ -34,51 +32,21 @@ public class PositionHandler implements PositionManager {
      * Calculate the positions of the nodes, and set the positions in each node.
      */
     public void calculatePositions() {
-        // Ugly and long code, works for us now however.
-        Random random = new Random();
-        int currx = 0;
-        Collection<Node> processed = new ArrayList<Node>();
-        // Detect and position snips.
-        for (Node node : nodeCollection) {
-            if (processed.contains(node)) {
-                continue;
-            }
-            Collection<Node> nodes = new ArrayList<Node>();
-            for (Node node2 : nodeCollection) {
-                Collection<Node> intersectionBack = new ArrayList<Node>(node2.getBackLinks());
-                intersectionBack.retainAll(node.getBackLinks());
-                Collection<Node> intersectionFront = new ArrayList<Node>(node2.getLinks());
-                intersectionFront.retainAll(node.getLinks());
-                if (!intersectionBack.isEmpty() && !intersectionFront.isEmpty()) {
-                    nodes.add(node2);
-                }
-            }
-            int height = nodes.size() * SPACING / 2;
-            for (Node node2 : nodes) {
-                node2.setXY(currx, height);
-                height -= SPACING;
-            }
-            processed.addAll(nodes);
-            currx += SPACING;
+        int posY = 0;
+        for (IGenome genome : genomes) {
+            constructBranches(genome, posY);
+            posY += 50;
         }
-        // Detect and correct the positions of indels.
-        for (Node node : nodeCollection) {
-            for (Node nodeBack : node.getBackLinks()) {
-                Collection<Node> intersection = new ArrayList<Node>(nodeBack.getLinks());
-                intersection.retainAll(node.getLinks());
-                if (!intersection.isEmpty()) {
-                    node.setXY(node.getX(), node.getY() + SPACING / 2);
-                }
-            }
-        }
-        // Remove the inversion links, until we know what to do with them.
-        for (Node node : nodeCollection) {
-            Iterator<Node> linkIterator = node.getLinks().iterator();
-            while (linkIterator.hasNext()) {
-                Node node2 = linkIterator.next();
-                if (node2.getX() < node.getX()) {
-                    linkIterator.remove();
-                }
+        int breakDebug = 0;
+    }
+
+
+    private void constructBranches(IGenome genome, int posY) {
+        Node node;
+        for (int nodeId : genome.getNodesIds()) {
+            node = nodeCollection.get(nodeId);
+            if (node.getCrdGenome().equals(genome.getName())) {//node.getOriGenomes().contains(genome.getName()) || 
+                node.setXY(node.getColumn()+100, posY);
             }
         }
     }
